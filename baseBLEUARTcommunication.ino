63a5a656ecb94a74bba7893b591ef60a8523692a
wlatcham <98995743+wlatcham@users.noreply.github.com>
2023-04-24 16:51:49 -0500

    Update baseBLEUARTcommunication.ino

--- baseBLEUARTcommunication.ino
+++ baseBLEUARTcommunication.ino
@@ -193,7 +193,7 @@ void decrypt(char password[], int key){
   for(int i=0; i<strlen(password); i++){
     password[i] = password[i] + key;
   }
-  void encrypt(char password[], int key){
+void encrypt(char password[], int key){
   for(int i=0;i,strlen(password);i++){
     password[i] = password[i] - key;
 }
