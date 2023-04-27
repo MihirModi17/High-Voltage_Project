93e8dd788d312de307f7e2b2734e938dbc62ff96
wyattkac <121465911+wyattkac@users.noreply.github.com>
2023-04-27 13:22:40 -0500

    Create encryptionHelper.ino

---
+++ encryptionHelper.ino
@@ -0,0 +1,42 @@
+#include <bluefruit.h>
+#include <Adafruit_LittleFS.h>
+#include <InternalFileSystem.h>
+#include <Adafruit_NeoPixel.h>
+
+const int MAXLENGTH = 64;
+
+void setup() {
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  char n[MAXLENGTH] = "test";
+  Serial.print(n);
+  Serial.print(" ");
+
+  //Encrypt the array and print to Serial
+  encrypt(n, 1);
+  Serial.print("{");
+  for(int i=0;i < strlen(n);i++)
+  {
+    Serial.print(int(n[i]));
+    Serial.print(", ");
+  }
+  Serial.print("'\\0'} ");
+
+  //Code to test if it works
+  //char k[MAXLENGTH] = {116, 101, 115, 116, '\0'};
+  //Serial.print(k);
+
+  Serial.println("");
+  delay(1000);
+  
+}
+
+void encrypt(char password[], int key){
+  for(int i=0;i < strlen(password);i++)
+  {
+    password[i] = password[i] - key;
+  }
+}
